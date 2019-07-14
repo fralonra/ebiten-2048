83b071e3a975f7b516ed5b48ad3d03a9f15f1dc0
fralonra <fralonra@aliyun.com>
2019-07-14 12:38:43 +0800

    init

--- game/game.go
+++ game/game.go
@@ -2,55 +2,101 @@ package game
 
 import (
 	"fmt"
+	"image/color"
+	"log"
+	"strconv"
+
+	"github.com/fralonra/ebiten-2048/assets/fonts"
 	"github.com/fralonra/go-2048/core"
+	"github.com/golang/freetype/truetype"
 	"github.com/hajimehoshi/ebiten"
 	"github.com/hajimehoshi/ebiten/ebitenutil"
-	"image/color"
+	"github.com/hajimehoshi/ebiten/inpututil"
+	"github.com/hajimehoshi/ebiten/text"
+	"golang.org/x/image/font"
 )
 
 const (
-	cellSize   = 80
+	cellSize   = 76
 	cellMargin = 4
 )
 
 var (
-	game      = core.NewGame()
-	cellImage *ebiten.Image
+	game       = core.NewGame()
+	frameColor = color.RGBA{0xbb, 0xad, 0xa0, 0xff}
+
+	cellImage       *ebiten.Image
+	mplusSmallFont  font.Face
+	mplusNormalFont font.Face
+	mplusBigFont    font.Face
 )
 
 type Game struct {
-	screen *ebiten.Image
+	cellTable [core.Size][core.Size]*ebiten.Image
+}
+
+func (g *Game) Setup() {
+
+}
+
+func (g *Game) Update(screen *ebiten.Image) {
+	g.checkKeyEvents()
+
+	if ebiten.IsDrawingSkipped() {
+		return
+	}
+
+	g.renderGame(screen)
+	g.checkState(screen)
 }
 
 func (g *Game) checkKeyEvents() {
-	if ebiten.IsKeyPressed(ebiten.KeyUp) {
+	if inpututil.IsKeyJustReleased(ebiten.KeyUp) {
 		game.ToTop()
 	}
-	if ebiten.IsKeyPressed(ebiten.KeyDown) {
+	if inpututil.IsKeyJustReleased(ebiten.KeyDown) {
 		game.ToBottom()
 	}
-	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
+	if inpututil.IsKeyJustReleased(ebiten.KeyLeft) {
 		game.ToLeft()
 	}
-	if ebiten.IsKeyPressed(ebiten.KeyRight) {
+	if inpututil.IsKeyJustReleased(ebiten.KeyRight) {
 		game.ToRight()
 	}
-	if ebiten.IsKeyPressed(ebiten.KeyR) {
+	if inpututil.IsKeyJustReleased(ebiten.KeyR) {
 		game = core.NewGame()
 	}
 }
 
 func (g *Game) renderGame(screen *ebiten.Image) {
-	for idx := 0; idx < core.Size; idx++ {
-		row := game.GetRow(idx)
-		for col, item := range row {
+	screen.Fill(frameColor)
+	for i := 0; i < core.Size; i++ {
+		row := game.GetRow(i)
+		for j, item := range row {
 			op := &ebiten.DrawImageOptions{}
-			x := idx*cellSize + (col+1)*cellMargin
-			y := col*cellSize + (idx+1)*cellMargin
+			x := j*(cellSize+cellMargin) + cellMargin
+			y := i*(cellSize+cellMargin) + cellMargin
 			op.GeoM.Translate(float64(x), float64(y))
 			r, g, b, a := colorToScale(getCellColor(item))
 			op.ColorM.Scale(r, g, b, a)
 			screen.DrawImage(cellImage, op)
+
+			if item != 0 {
+				str := strconv.Itoa(item)
+				f := mplusBigFont
+				switch {
+				case 3 < len(str):
+					f = mplusSmallFont
+				case 2 < len(str):
+					f = mplusNormalFont
+				}
+				bound, _ := font.BoundString(f, str)
+				w := (bound.Max.X - bound.Min.X).Ceil()
+				h := (bound.Max.Y - bound.Min.Y).Ceil()
+				x = x + (cellSize-w)/2
+				y = y + (cellSize-h)/2 + h
+				text.Draw(screen, str, f, x, y, color.White)
+			}
 		}
 	}
 }
@@ -72,12 +118,6 @@ func (g *Game) checkState(screen *ebiten.Image) {
 			ebitenutil.DebugPrint(screen, "You lost! Press 'r' to restart")
 		}
 	}
-}
-
-func (g *Game) Update(screen *ebiten.Image) {
-	g.checkKeyEvents()
-	g.renderGame(screen)
-	g.checkState(screen)
 }
 
 func getCellColor(value int) color.Color {
@@ -128,4 +168,26 @@ func colorToScale(clr color.Color) (float64, float64, float64, float64) {
 func init() {
 	cellImage, _ = ebiten.NewImage(cellSize, cellSize, ebiten.FilterDefault)
 	cellImage.Fill(color.White)
+
+	tt, err := truetype.Parse(fonts.MPlus1pRegular_ttf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	const dpi = 72
+	mplusSmallFont = truetype.NewFace(tt, &truetype.Options{
+		Size:    24,
+		DPI:     dpi,
+		Hinting: font.HintingFull,
+	})
+	mplusNormalFont = truetype.NewFace(tt, &truetype.Options{
+		Size:    32,
+		DPI:     dpi,
+		Hinting: font.HintingFull,
+	})
+	mplusBigFont = truetype.NewFace(tt, &truetype.Options{
+		Size:    48,
+		DPI:     dpi,
+		Hinting: font.HintingFull,
+	})
 }
--- main.go
+++ main.go
@@ -7,20 +7,17 @@ import (
 )
 
 var (
-	g *game.Game
+	g = &game.Game{}
 )
 
 func update(screen *ebiten.Image) error {
-	if ebiten.IsDrawingSkipped() {
-		return nil
-	}
-
 	g.Update(screen)
 	return nil
 }
 
 func main() {
-	if err := ebiten.Run(update, 320, 320, 2, "2048"); err != nil {
+	if err := ebiten.Run(update, 324, 324, 2, "2048"); err != nil {
 		log.Fatal(err)
 	}
+	g.Setup()
 }
